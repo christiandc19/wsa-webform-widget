b9a2133562b379699443a1dd417946f9278ed133
github_christiandc19 <christiandc19@hotmail.com>
2026-05-20 16:35:10 -0700

    added clients

---
+++ src/webforms/clients/robinRun.js
@@ -0,0 +1,28 @@
+const robinRunWebform = {
+  clientKey: "robin-run",
+
+  branding: {
+    title: "Contact Robin Run Village",
+    subtitle:
+      "Tell us what you need, and our team will follow up with the right next step.",
+  },
+
+  theme: {
+    primaryColor: "#19606f",
+  },
+
+  security: {
+    recaptchaSiteKey: "6Lf4UdssAAAAAOChTYz1U_qvNYLK9a2fkTVJ55lI",
+  },
+
+  forms: {
+    "senior-living-contact": {
+      formKey: "senior-living-contact",
+      title: "Contact Us",
+      submitLabel: "Submit",
+      successMessage: "Thank you! We’ll be in touch.",
+    },
+  },
+};
+
+export default robinRunWebform;
